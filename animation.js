5ac9ab63b4831cfcd2a13dba6f446369111ea3b7
Bryan <bryantirta23@gmail.com>
2024-04-10 18:43:47 +0800

    first

---
+++ animation.js
@@ -0,0 +1,8 @@
+// Script untuk animasi menu
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
